a104860141a0870b76fb445b01be4142922e574d
mirkoron <mirkoron1@virgilio.it>
2016-01-28 14:25:22 +0100

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,12 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ 
+ var awesomeThoughts = " I am Mirko and I am awesome!";
+console.log(awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace("awesome","fun");
+
+$("#main").append(funThoughts);
+
+console.log(funThoughts);
